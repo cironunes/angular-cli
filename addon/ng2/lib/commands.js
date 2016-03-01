b8d91fbe3c8bc2677d6aea09a2c0fa6005c7c379
Ciro Nunes <ciroanunes@gmail.com>
2016-03-01 16:38:11 +0100

    refactor(commands): move it to a proper module

---
+++ addon/ng2/lib/commands.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+    return {
+      'new'       : require('../commands/new'),
+      'init'      : require('../commands/init'),
+      'test'      : require('../commands/test'),
+      'e2e'       : require('../commands/e2e'),
+      'lint'      : require('../commands/lint'),
+      'format'    : require('../commands/format'),
+      'version'   : require('../commands/version'),
+      'completion': require('../commands/completion')
+    };
+};
